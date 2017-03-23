d6380d20682cd8b4c09d799a657c3b32a87a5ccf
Ashwin Parmar <ashwin.d.parmar@accenture.com>
2017-03-23 19:55:35 +0000

    Custom Module developed for Product Listing block

--- htdocs/modules/custom/nestle_general_web_parts/src/Plugin/Block/ProductCanvasHeaderImageBlock.php
+++ htdocs/modules/custom/nestle_general_web_parts/src/Plugin/Block/ProductCanvasHeaderImageBlock.php
@@ -6,6 +6,8 @@
 namespace Drupal\nestle_general_web_parts\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+use \Drupal\Core\Routing;
 
 /**
  * Provides a 'Hello' Block.
@@ -24,8 +26,41 @@ class ProductCanvasHeaderImageBlock extends BlockBase {
   public function build() {
     return array(
 		'#type' => 'markup',
-		'#markup' => $this->t('Hello, World!'),
+		'#theme' => 'hello_block',
+		
+		'#content' => $this->getProducts(),
+		
+		'#cache' => array('max-age' => 0)
     );
+  }
+  
+  private function getProducts() {
+		$result = array();
+		$city_data = \Drupal::request()->attributes->all()['taxonomy_term']->toArray();
+		$city_id = \Drupal::request()->attributes->all()['taxonomy_term']->id();
+		$country_id = !empty($city_data['field_img_city']) ? $city_data['field_img_city'][0]['target_id'] : 0;
+		
+		$query = \Drupal::entityQuery('node')->condition('type', 'products_page')->range(0,3)->execute();
+		
+		
+		if ($query) {
+			foreach($query as $nid) {
+				$node = Node::load($nid);
+				#$node = Node::load($nid)->toArray();
+				
+				#$result[]['nid'] = $node->Id();
+				$result[]['title'] = $node->getTitle();
+				$result[]['image_data'] = $node->get('field_product_image')->getValue();
+					
+					#$file = $node->get('field_product_image')->getValue();
+					#$file = \Drupal\file\Entity\File::load($file);
+
+				#print "<pre>";print_r($file); die;
+			}
+		}
+		
+		#print "<pre>";print_r($result[0]); die;
+		return $result;
   }
 
 }
