d064f3037a2ef585044ae8fc12de85e1029ccfd9
Ashwin Parmar <ashwin.d.parmar@accenture.com>
2017-03-27 19:47:06 +0000

    Global Header / Footer set

--- htdocs/modules/custom/nestle_general_web_parts/src/Plugin/Block/CityPageHeaderBlock.php
+++ htdocs/modules/custom/nestle_general_web_parts/src/Plugin/Block/CityPageHeaderBlock.php
@@ -43,16 +43,21 @@ class CityPageHeaderBlock extends BlockBase {
 	$language_code = \Drupal::languageManager()->getCurrentLanguage()->getId();
 	// Language Name
 	$language_name =  \Drupal::languageManager()->getCurrentLanguage()->getName();
+	// Term ID From URL
+	$current_path = \Drupal::service('path.current')->getPath();
+	$path_args 	  = explode('/', $current_path);
+	$city_country_tid = isset($path_args[3]) ? $path_args[3] : 0;
+	#print "<pre>";print_r($path_args); die;
 	
-	$query = \Drupal::entityQuery('taxonomy_term')->condition('vid', 'country')->execute();
-	
-	if ($query) {
-		
-		foreach($query as $key=>$tid) {
-			$country_term = \Drupal\taxonomy\Entity\Term::load($tid);			
-		}
+	if ($city_country_tid) {
+		$term = \Drupal\taxonomy\Entity\Term::load($city_country_tid);
+		$result['title'] = "Taste Guide";
+		$result['image_data'] = $term->get('field_img_city')->getValue();	// Image Object
+		$result['image_url'] = ImageStyle::load('header_image_1680x408')->buildUrl(File::load($term->get('field_img_city')->getValue()[0]['target_id'])->getFileUri());	// Image URL
+		$result['city_title'] = trim($term->toLink()->getText());
+		$result['city_text'] = trim($term->getDescription());
 	}
-	
+
 	#print "<pre>";print_r($result); die;
 	return $result;
   }
---
+++ htdocs/modules/custom/nestle_general_web_parts/src/Plugin/Block/PageFooterBlock.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ */
+namespace Drupal\nestle_general_web_parts\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+use \Drupal\Core\Routing;
+use \Drupal\Core\Url;
+use Drupal\image\Entity\ImageStyle;
+use \Drupal\file\Entity\File;
+
+/**
+ * Provides a 'Page Footer Block' Block.
+ *
+ * @Block(
+ *   id = "page_footer_block",
+ *   admin_label = @Translation("Page Footer Block"),
+ *   category = @Translation("Nestle"),
+ * )
+ */
+class PageFooterBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+		'#type' => 'markup',
+		'#theme' => 'page_footer_block',
+		'#content' => $this->getContent(),
+		'#cache' => array('max-age' => 0)
+    );
+  }
+  
+  private function getContent() {
+	$result = array();
+	return $result;
+  }
+
+}
